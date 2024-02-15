c569458a8c6136c4a1b156b99802c5830c11b19f
sethmoney <seth.money@gmail.com>
2024-02-15 18:18:56 -0500

    Update player.js
    added code for player.js

--- player.js
+++ player.js
@@ -1,1 +1,20 @@
+// player.js
+function handlePlayerInput() {
+    if (keyIsDown(87)) { // W key
+        playerPosition.y -= 5;
+        if (playerPosition.y < 0) playerPosition.y = height;
+    }
+    if (keyIsDown(83)) { // S key
+        playerPosition.y += 5;
+        if (playerPosition.y > height) playerPosition.y = 0;
+    }
+    if (keyIsDown(65)) { // A key
+        playerPosition.x -= 5;
+        if (playerPosition.x < 0) playerPosition.x = width;
+    }
+    if (keyIsDown(68)) { // D key
+        playerPosition.x += 5;
+        if (playerPosition.x > width) playerPosition.x = 0;
+    }
+}
 
